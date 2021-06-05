abe3bd1edfb786829b29417c144cb27467277fc3
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-06-05 18:15:05 -0300

    criando model categoria

---
+++ database/models/Categoria.js
@@ -0,0 +1,24 @@
+const { Sequelize } = require("sequelize/types");
+
+module.exports = (sequelize, DataType) => {
+    const Categoria = sequelize.define('Categoria', {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome: DataType.STRING
+    }, {
+        tableName: 'categorias',
+        timestamps: false
+    })
+
+    Categoria.associate = (model) => {
+        Categoria.hasMany(model.Filme, {
+            foreignKey: 'fk_categoria',
+            as: 'filmes'
+        })
+    }
+
+    return Categoria
+}
